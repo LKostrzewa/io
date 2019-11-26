84ee14d646e82d1ed30167312a04013d4a74ea78
PawelBialek <216723@edu.p.lodz.pl>
2019-11-26 22:14:29 +0100

    Dodano dodawanie wydarzen, ktore juz dziala jak Pan Jezus powiedzial

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.sqlite'
@@ -27,22 +28,21 @@ class User(db.Model):
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80))
     # nwm jak tutaj wiele uzytkownikow zrobic
     # users = db.relationship("User", backref = "event")
-    description = db.Column(db.String(80), nullable=False)
-    date = db.Column(db.DATE, nullable=False)
-    time = db.Column(db.TIME, nullable=False)
+    description = db.Column(db.String)
+    date = db.Column(db.DateTime)
+    # time = db.Column(db.Time)
     # auditorium = db.Column(db.Integer, db.ForeignKey('auditorium.id'))
     # tags = db.relationship('User', secondary=tags, lazy='subquery',
     #                        backref=db.backref('event', lazy=True))
 
-    def __init__(self, id, name, description, date, time):#, auditorium, tags):
+    def __init__(self, id, name, description, date):#, auditorium, tags):
         self.id = id
         self.name = name
         self.description = description
         self.date = date
-        self.time = time
         # self.auditorium = auditorium
         # self.tags = tags
 
@@ -79,9 +79,10 @@ def add_event():
         print(request.form['name'])
         print(request.form['desc'])
         print(request.form['date'])
-        print(request.form['time'])
-        db.session.add(Event(id=request.form['id'], name=request.form['name'], description=request.form['desc'],
-                             date=request.form['date'], time=request.form['time']))
+        data = datetime.datetime(*[int(v) for v in request.form['date'].replace('T', '-').replace(':', '-').split('-')])
+        event = Event(id=request.form['id'], name=request.form['name'], description=request.form['desc'],
+                      date=data)
+        db.session.add(event)
                              #, request.form['auditorium'],
                              # request.form['tags']))
         db.session.commit()
